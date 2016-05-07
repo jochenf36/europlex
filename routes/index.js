ec84e46d7da72970e5ea2a94f6908b4c86dceca9
Jochen <jochen.francois@gmail.com>
2016-05-07 13:34:10 +0200

    sort images

--- routes/index.js
+++ routes/index.js
@@ -4,36 +4,40 @@ var resources = require('../resources.js');
 var logger = require('../logger.js');
 
 var cards = [];
+var sortedCards;
 
 function getCards() {
   return resources.getEntries({
     content_type: 'cardWithImageAndText',
-    order: {
-      title: true,
-    },
   });
 }
 
-getCards().then(function resolver(res) {
-  res.items.forEach(function fillCards(item, index) {
-    cards[index] = {
-      title: item.fields.title,
-      image: item.fields.image.fields.file.url,
-    };
-  });
-}).catch(logger.log);
+function getData(res, req) {
+  getCards().then(function resolver(result) {
+    result.items.forEach(function fillCards(item, index) {
+      cards[index] = {
+        title: item.fields.title,
+        image: item.fields.image.fields.file.url,
+        order: item.fields.order,
+      };
+    });
+    sortedCards = cards.sort(function sort(a, b) {
+      return a.order - b.order;
+    });
+    res.render('index', {
+      title: req.i18n.__('title'),
+      about: req.i18n.__('about'),
+      contact: req.i18n.__('contact'),
+      location: req.i18n.__('location'),
+      lang: req.i18n.getLocale(),
+      cards: sortedCards,
+    });
+  }).catch(logger.log);
+}
 
-console.log(cards);
 /* GET home page. */
 router.get('/', function index(req, res) {
-  res.render('index', {
-    title: req.i18n.__('title'),
-    about: req.i18n.__('about'),
-    contact: req.i18n.__('contact'),
-    location: req.i18n.__('location'),
-    lang: req.i18n.getLocale(),
-    cards: cards,
-  });
+  getData(res, req);
 });
 
 module.exports = router;
